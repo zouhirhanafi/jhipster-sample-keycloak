084a9f0fef9c265971550848fcafcbf0eb48ae6a
Zouhir HANAFI <zouhir.hanafi@gmail.com>
2022-03-17 13:31:15 +0100

    web_app pointe sur nextjs 3000

--- next-auth-example/pages/api/auth/[...nextauth].ts
+++ next-auth-example/pages/api/auth/[...nextauth].ts
@@ -34,10 +34,10 @@ export default NextAuth({
     //   clientId: process.env.FACEBOOK_ID,
     //   clientSecret: process.env.FACEBOOK_SECRET,
     // }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID,
+    //   clientSecret: process.env.GITHUB_SECRET,
+    // }),
     KeycloakProvider({
       clientId: process.env.KEYCLOAK_ID,
       clientSecret: process.env.KEYCLOAK_SECRET,
